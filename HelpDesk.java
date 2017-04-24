ffadc58231c902dbabbbdfbb8de98f2396e7141b
Gillgamesh <ggill@stuy.edu>
2017-04-24 01:10:18 -0400

    Working version

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+//import java linkedlist as an archive
+import java.util.List;
+import java.util.LinkedList;
 public class HelpDesk {
+    //actual tickets
     private PriorityQueue<Ticket> tickets;
+    //archive of tickets, could be any datatype
+    private List<Ticket> archive;
     private int tick_count;
     public HelpDesk() {
         tickets = new ArrayPriorityQueue<Ticket>();
+        archive = new LinkedList<Ticket>();
         tick_count= 0;
     }
 
-
+    public boolean isEmpty() {
+        return tickets.isEmpty();
+    }
     public void openTicket() {
         //generate an id: just have it be an integer
-        String id = "" + (++tick_count);
+        int id = (++tick_count);
         //ticket needs the following information:
 
         //Ask them their name
@@ -24,23 +33,60 @@ public class HelpDesk {
         //Ask if theres someone else to create another ticket
     }
 
+    //close a ticket, specify the message to close it with. Closing it effectively means take them off the queue, and store it in another priority queue
+    public void closeTicket(String solution, boolean isSolved) {
+        Ticket ticket = tickets.popMin();
+        //set the tickets solution to whatever it was, and state whether it was solved or not.
+        ticket.close(solution, isSolved);
+        //now, you can add it to the list containing the closed tickets
+        archive.add(ticket);
 
+    }
+
+    //answer a ticket and close it when done
     public void doTicket() {
+        Ticket ticket = tickets.peekMin();
+        String problem = ticket.getProblem().toLowerCase();
+        //if it isn't a computer problem, then wrong place.
+        if ((problem.indexOf("pc") == -1) && (problem.indexOf("computer") == -1) && (problem.indexOf("mac") == -1)) {
+            System.out.println("Dear " + ticket.getName() + ", we cannot solve your problem. Sorry");
+            closeTicket("Not a computer problem, can't fix", false);
+            return;
+        }
+        //if their computer is working, great job problem solved
+        if (YoN("Is your computer working, "+ ticket.getName())) {
+            closeTicket("Computer is functional", true);
+        }
+        //if not, have them turn it on and off again
+        else if (!YoN("Have you tried turning it on and off again or ensuring it's plugged in")) {
+            System.out.println("Try that");
+            //restart the question loop
+            doTicket();
+            return;
+        }
+        else {
+            System.out.println("Sorry, we can't help you any further.");
+            closeTicket("Too complicated", false);
+        }
+        System.out.println("Thanks, come again!");
 
     }
 
 
 
     //USER INPUT METHODS
+    //returns the thing the user typed in
     public String promptWord(String s) {
         System.out.print(s+": ");
         Scanner sc = new Scanner(System.in);
-        while (!sc.hasNext()) {
+        while (!sc.hasNextLine()) {
         }
         System.out.print("\n");
-        return sc.next();
+        //this just gets their first name
+        return sc.nextLine();
 
     }
+    //gets an int
     public int promptInt(String s) {
         System.out.print(s+": ");
         Scanner sc = new Scanner(System.in);
@@ -52,7 +98,7 @@ public class HelpDesk {
     }
     //asks yes or no, returns true or falses
     public boolean YoN(String s) {
-        String ans = promptWord(s);
+        String ans = promptWord(s).toLowerCase();
         //keep prompting them until their answer contains yes or no.
         while ((ans.indexOf("yes") == -1) &&
         (ans.indexOf("no") == -1)) {
@@ -69,6 +115,8 @@ public class HelpDesk {
         HelpDesk desk = new HelpDesk();
         while (desk.YoN("Does someone have a ticket")) desk.openTicket();
 
+        //now that you have your tickets, do them until the priority queue is empty
+        while (!desk.isEmpty()) desk.doTicket();
     }
 
 }
--- Ticket.java
+++ Ticket.java
@@ -1,15 +1,16 @@
 public class Ticket implements Comparable<Ticket> {
-    private String _id;
+    private int _id;
     private boolean _solved;
     private int _priority;
     private String _name;
     private String _problem;
     private String _solution;
-    public Ticket(String id, String name, int priority, String problem) {
+    public Ticket(int id, String name, int priority, String problem) {
         _name = name;
         _id = id;
         _priority = priority;
         _problem = problem;
+        _solved = false;
     }
     //the one with lower priority will have a return a lower value because the APQ works in Minimum first order
     public int compareTo(Ticket other) {
@@ -17,5 +18,25 @@ public class Ticket implements Comparable<Ticket> {
     }
     public int getPriority() {
         return _priority;
+    }
+    public String getProblem() {
+        return _problem;
+    }
+    public String getSolution() {
+        return _solution;
+    }
+    public boolean isSolved() {
+        return _solved;
+    }
+    public int getId() {
+        return _id;
+    }
+    public String getName() {
+        return _name;
+    }
+
+    public void close(String s, boolean solved) {
+        _solution = s;
+        _solved = solved;
     }
 }
